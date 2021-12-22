f9a5827a793af9ea6df10a98a0c6785050e30036
3emo <mohamed.emadd91@gmail.com>
2021-12-22 19:44:35 +0200

    fixing some problems

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,4 @@
 import icons from 'url:../../img/icons.svg';
-const Fraction = require('../../../node_modules/fractional/index.js');
 import View from './view.js';
 
 class RecipeView extends View {
@@ -100,9 +99,7 @@ class RecipeView extends View {
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity).toString() : ''
-        }</div>
+        <div class="recipe__quantity">${ing.quantity ? ing.quantity : ''}</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
           ${ing.description}
